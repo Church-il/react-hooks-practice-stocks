c12f8aa32cfd391652bc89a27bd12d5e3e4021d7
Michael Akoko <akokomichael37@gmail.com>
2024-09-08 21:50:57 +0300

    insert current scripts

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
 
 function App() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then(response => response.json())
+      .then(data => setStocks(data));
+  }, []);
+
+  const buyStock = (stock) => {
+    if (!portfolio.find(s => s.id === stock.id)) {
+      setPortfolio([...portfolio, stock]);
+    }
+  };
+
+  const sellStock = (stock) => {
+    setPortfolio(portfolio.filter(s => s.id !== stock.id));
+  };
+
   return (
     <div>
       <Header />
-      <MainContainer />
+      <MainContainer
+        stocks={stocks}
+        portfolio={portfolio}
+        buyStock={buyStock}
+        sellStock={sellStock}
+      />
     </div>
   );
 }
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
-function MainContainer() {
+function MainContainer({ stocks, portfolio, buyStock, sellStock }) {
+  const [sortBy, setSortBy] = useState("");
+  const [filterBy, setFilterBy] = useState("");
+
+  const sortedStocks = [...stocks].sort((a, b) => {
+    if (sortBy === "Alphabetically") {
+      return a.ticker.localeCompare(b.ticker);
+    } else if (sortBy === "Price") {
+      return a.price - b.price;
+    }
+    return 0;
+  });
+
+  const filteredStocks = sortedStocks.filter(stock => 
+    filterBy === "" || stock.type === filterBy
+  );
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+        setFilterBy={setFilterBy}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={filteredStocks} buyStock={buyStock} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolio={portfolio} sellStock={sellStock} />
         </div>
       </div>
     </div>
